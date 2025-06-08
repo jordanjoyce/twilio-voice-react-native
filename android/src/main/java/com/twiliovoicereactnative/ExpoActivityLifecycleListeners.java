ccb571046a8066b0a778825ae7ff98b93f9ae59d
Jordan Joyce <jordan@jordanjoyce.com.au>
2025-06-08 16:45:20 +1000

    Fix lifestyle permissions

--- android/src/main/java/com/twiliovoicereactnative/ExpoActivityLifecycleListeners.java
+++ android/src/main/java/com/twiliovoicereactnative/ExpoActivityLifecycleListeners.java
@@ -1,8 +1,12 @@
 package com.twiliovoicereactnative;
 
+import android.Manifest;
+import android.os.Build;
+import android.widget.Toast;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+
 
 import expo.modules.core.interfaces.ReactActivityLifecycleListener;
 
@@ -15,19 +19,19 @@ public class ExpoActivityLifecycleListeners implements ReactActivityLifecycleLis
     this.voiceActivityProxy = new VoiceActivityProxy(activity, permission -> {
       if (Manifest.permission.RECORD_AUDIO.equals(permission)) {
         Toast.makeText(
-          MainActivity.this,
+          activity,
           "Microphone permissions needed. Please allow in your application settings.",
           Toast.LENGTH_LONG).show();
       } else if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) &&
         Manifest.permission.BLUETOOTH_CONNECT.equals(permission)) {
         Toast.makeText(
-          MainActivity.this,
+          activity,
           "Bluetooth permissions needed. Please allow in your application settings.",
           Toast.LENGTH_LONG).show();
       } else if ((Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) &&
         Manifest.permission.POST_NOTIFICATIONS.equals(permission)) {
         Toast.makeText(
-          MainActivity.this,
+          activity,
           "Notification permissions needed. Please allow in your application settings.",
           Toast.LENGTH_LONG).show();
       }
