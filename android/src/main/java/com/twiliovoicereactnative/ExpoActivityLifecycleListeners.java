52b6a6fab4a60b51aaf5fec0e220aba8dfecaf59
Jordan Joyce <jordan@jordanjoyce.com.au>
2025-06-08 14:16:01 +1000

    Implment permissions

--- android/src/main/java/com/twiliovoicereactnative/ExpoActivityLifecycleListeners.java
+++ android/src/main/java/com/twiliovoicereactnative/ExpoActivityLifecycleListeners.java
@@ -12,7 +12,27 @@ public class ExpoActivityLifecycleListeners implements ReactActivityLifecycleLis
 
   @Override
   public void onCreate(Activity activity, Bundle savedInstanceState) {
-    this.voiceActivityProxy = new VoiceActivityProxy(activity, null);
+    this.voiceActivityProxy = new VoiceActivityProxy(activity, permission -> {
+      if (Manifest.permission.RECORD_AUDIO.equals(permission)) {
+        Toast.makeText(
+          MainActivity.this,
+          "Microphone permissions needed. Please allow in your application settings.",
+          Toast.LENGTH_LONG).show();
+      } else if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) &&
+        Manifest.permission.BLUETOOTH_CONNECT.equals(permission)) {
+        Toast.makeText(
+          MainActivity.this,
+          "Bluetooth permissions needed. Please allow in your application settings.",
+          Toast.LENGTH_LONG).show();
+      } else if ((Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) &&
+        Manifest.permission.POST_NOTIFICATIONS.equals(permission)) {
+        Toast.makeText(
+          MainActivity.this,
+          "Notification permissions needed. Please allow in your application settings.",
+          Toast.LENGTH_LONG).show();
+      }
+    });
+    
     this.voiceActivityProxy.onCreate(savedInstanceState);
   }
 
